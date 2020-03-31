7142874156a2327829b5778ff11c1356889553d3
David Warne <warned@qut.edu.au>
2020-04-01 09:45:24 +1000

    updated data import
    Now deals with new data format. The data import now does not assume
    any particular column ordering and will ignore additional columns.

--- run_smc_intensity_reg.m
+++ run_smc_intensity_reg.m
@@ -27,32 +27,62 @@
 %clear global
 % country_id defined within the current workspace to select the region to perform
 % inference on.
-rng(mod(1337,country_id),'twister')
+rng(1337,'twister')
 
-% data store
+%%
+region_id = 'China'; % 
+iso_id = 'CHN';
+province_id = 'total';
+
+%% data store
 DATA_DIR = '../Data/'
 
 % load transition matrix for confirmed cases by country
-T_d = import_covid19data([DATA_DIR,'COVID19data/data-raw/covid19_sorted.csv']);
-JHU_ISO3166 = import_JHUtoA3([DATA_DIR,'COVID-19_ISO-3166/JohnsHopkins-to-A3.csv']);
-P_d = import_country_full_list([DATA_DIR,'COVID-19_ISO-3166/full_list.csv']); 
+opts = detectImportOptions([DATA_DIR,'COVID19data/data-raw/covid19_sorted.csv']);
+% note: this is a bit verbose, but it is the most effective way I can get
+% to ensure the order and number of columns is arbitrary
+for i=1:length(opts.VariableNames)
+    switch opts.VariableNames{i}
+        case 'alpha3'
+            opts.VariableTypes{i} = 'categorical';
+        case 'Country_Region'
+            opts.VariableTypes{i} = 'categorical';
+        case 'Province_State'
+            opts.VariableTypes{i} = 'categorical';
+        case 'date'
+            opts.VariableTypes{i} = 'datetime';
+        otherwise
+            opts.VariableTypes{i} = 'double';
+    end
+end
+T_d = readtable([DATA_DIR,'COVID19data/data-raw/covid19_sorted.csv'],opts);
 
-% extract table for Country/Region (e.g., define country_id = 44 for china, or 114 for Italy)
-T_country_id = T_d(ismember(T_d.CountryRegion,...
-                            JHU_ISO3166.CountryRegion(...
-                            JHU_ISO3166.alpha3 == P_d.alpha3(country_id))) & ...
-                            T_d.ProvinceState == 'total',:);
-
-
+% load population table
+opts = detectImportOptions([DATA_DIR,'COVID-19_ISO-3166/full_list.csv']);
+% note: this is a bit verbose, but it is the most effective way I can get
+% to ensure the order and number of columns is arbitrary
+for i=1:length(opts.VariableNames)
+    switch opts.VariableNames{i}
+        case 'alpha3'
+            opts.VariableTypes{i} = 'categorical';
+        case 'Province_State'
+            opts.VariableTypes{i} = 'categorical';
+        case 'population'
+            opts.VariableTypes{i} = 'double';
+    end
+end
+P_d = readtable([DATA_DIR,'COVID-19_ISO-3166/full_list.csv'],opts);
 
 %% extract region of interest
-Data.C = T_country_id.deaths;
-Data.R = T_country_id.recovered;
-Data.D = T_country_id.active;
-Data.P = P_d.population(country_id);
+I = T_d.alpha3 == iso_id & T_d.Province_State == province_id;
+Data.D = T_d.deaths(I);
+Data.R = T_d.recovered(I);
+Data.C = T_d.active(I);
+J = P_d.alpha3 == iso_id & P_d.Province_State == province_id;
+Data.P = P_d.population(J);
 
 % check simulation is viable
-if Data.P <= 0 || sum(Data.C > 0) == 0 || P_d.alpha3(country_id) == 'cruise'
+if Data.P <= 0 || sum(Data.C > 0) == 0 || P_d.alpha3(J) == 'cruise'
     % unpopulated region or no cases of COVID-19 reported
     return
 end
@@ -102,12 +132,12 @@ results.sims = sims;
 results.epsilon = epsilon_t;
 results.p_acc_t = p_acc_t;
 results.data = Data;
-results.name = char(P_d.CountryRegion(country_id))
-results.ISO3166alpha3 = char(P_d.alpha3(country_id))
+results.name = sprintf('%s%s',region_id,province_id);
+results.ISO3166alpha3 = iso_id;
 % save output
-save(['results_smc_intensity_poisson_reg',num2str(country_id),'_',results.ISO3166alpha3,'.mat'],'results');
+save(['results_smc_intensity_poisson_reg',results.name,'_',results.ISO3166alpha3,'.mat'],'results');
 %% load output 
-load(['results_smc_intensity_poisson_reg',num2str(country_id),'_',results.ISO3166alpha3,'.mat'],'results');
+load(['results_smc_intensity_poisson_reg',results.name,'_',results.ISO3166alpha3,'.mat'],'results');
 
 % plot marginal posterior densities
 figure;
@@ -118,9 +148,9 @@ for i = 1:8
     xlim([prior.p1(i),prior.p2(i)])
     xlabel(lab{i})
 end
-title(sprintf('%d %s %s',country_id,results.name,results.ISO3166alpha3))
+title(sprintf('%s %s',results.name,results.ISO3166alpha3))
 
-% plot samples of posterior predicitve distribution agaist data
+% plot samples of posterior predictve distribution against data
 figure;
 T = length(results.data.C);
 for i=1:1000
@@ -129,7 +159,7 @@ for i=1:1000
     hb = plot(results.part_sim(i,T+1:2*T)','-','Color',[237,177,32]/255); hb.Color(4) = 0.05;
     hc = plot(results.part_sim(i,2*T+1:3*T)','-r'); hc.Color(4) = 0.05;
 end
-title(sprintf('%d %s %s',country_id,results.name,results.ISO3166alpha3))
+title(sprintf('%s %s',results.name,results.ISO3166alpha3))
 plot(results.data.C,'+k');
 plot(results.data.R,'+k');
 plot(results.data.D,'+k');
